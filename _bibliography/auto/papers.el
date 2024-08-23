64ab1b216c4293c01cb8976ef06ac197195bd095
Arun Kumar <dzrarunkumar@gmail.com>
2024-08-23 11:56:59 +0530

    editing .bib file

---
+++ _bibliography/auto/papers.el
@@ -0,0 +1,10 @@
+(TeX-add-style-hook
+ "papers"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "kumar2023asymptotic"
+    "kumar2024wavelength"
+    "kumar2020investigation"
+    "kumar2021more"))
+ :bibtex)
+
